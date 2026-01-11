68bf4ccbc4dd993c88110770b222e09811c3cb77
Mas Tomi <anoboy2000@gmail.com>
2026-01-11 19:40:52 +0700

    Update portfolio: add new projects (Lumichan AI, Project PKM, Shiraori Joki V2, Traffic RNN)

---
+++ Portfolio-Tomi-main/src/App.vue
@@ -0,0 +1,145 @@
+<template>
+  <!-- component -->
+  <!-- This is an example component -->
+  <div class="max-w-7xl mx-auto flex flex-col relative">
+
+    <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
+      <div class="container mx-auto flex flex-wrap items-center justify-between">
+        <button @click="redirectToHome" class="flex">
+          <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">Mas Tomi :V</span>
+        </button>
+        <div class="flex md:order-2 fadein-bot">
+          <a href="https://github.com/Tomi-012" class="hover:opacity-80 transition-opacity text-white">
+            <svg class="w-9 h-9" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+            </svg>
+          </a>
+        </div>
+        <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
+          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <router-link to="/"
+                class="fadein-bot text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0"
+                aria-current="page">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about"
+                class="fadein-bot fadein-1 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">About</router-link>
+            </li>
+            <li>
+              <router-link to="/portfolio"
+                class="fadein-bot fadein-2 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0">Portfolio</router-link>
+            </li>
+            <li>
+              <router-link to="/blog"
+                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="md:mt-[100px]">
+      <router-view />
+    </div>
+  </div>
+  <footer class="block md:hidden fixed bottom-0 left-0 right-0 rounded-t-3xl border border-[#383838] bg-[#121212] bg-opacity-80 backdrop-blur-md backdrop-opacity-90">
+  <nav class="flex justify-around py-4 text-xs">
+    <router-link to="/" class="text-gray-300 hover:text-white">Home</router-link>
+    <router-link to="/about" class="text-gray-300 hover:text-white">About</router-link>
+    <router-link to="/portfolio" class="text-gray-300 hover:text-white">Portfolio</router-link>
+    <router-link to="/blog" class="text-gray-300 hover:text-white">Blog</router-link>
+  </nav>
+</footer>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToHome() {
+      this.$router.push('/')
+    }
+  },
+}
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+  width: 5px; /* for vertical scrollbar */
+  height: 5px; /* for horizontal scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: hsl(240, 1%, 17%);
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #ffdb70;
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-button { width: 20px; }
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background: hsl(0, 0%, 7%);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active {
+  display: inline-flex;
+  flex-direction: column;
+  color: white;
+  transition: color 0.3s;
+}
+
+nav a.router-link-exact-active::after {
+  display: inline-block;
+  content: "";
+  margin-top: 0.08em;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: #ffdb70;
+}
+
+nav a.router-link-exact-active:hover {
+  color: white;
+}
+
+
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+</style>
